dae01909025b6d314e26ad6d7ae140e019aa1ae3
S. Larsson <post@slarsson.de>
2025-01-23 16:43:23 +0000

    Update Trackdetails

--- pages/05_Track-Database.py
+++ pages/05_Track-Database.py
@@ -26,20 +26,20 @@ if st.button("Track suchen"):
 
     with col1:
         st.subheader("Titel:")
-        st.write(trackdetails(track_input, artist_input, "Track", df))
+        st.write(trackdetails(track_input, artist_input, 'Track', df))
         st.subheader("Release Year:")
-        st.write(trackdetails(track_input, artist_input, "Release Year", df))
+        st.write(trackdetails(track_input, artist_input, 'Release Year', df))
         st.subheader("Explicit Track:")
-        st.write(trackdetails(track_input, artist_input, "Explicit Track", df))
+        st.write(trackdetails(track_input, artist_input, 'Explicit Track', df))
 
 
     with col2:
         st.subheader("Interpret:")
-        st.write(trackdetails(track_input, artist_input, "Artist", df))
+        st.write(trackdetails(track_input, artist_input, 'Artist', df))
         st.subheader("Album:")
-        st.write(trackdetails(track_input, artist_input, "Album", df))
+        st.write(trackdetails(track_input, artist_input, 'Album', df))
         st.subheader("Popularity:")
-        st.write(f"{(trackdetails(track_input, artist_input, "Spotify Popularity", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'Spotify Popularity', df)):,.0f}")
 
     with col3:
         search_img(artist_input, 'Track-Bild', df)
@@ -52,21 +52,21 @@ if st.button("Track suchen"):
 
     with col4:
         st.subheader("Spotify:")
-        st.write(f"{(trackdetails(track_input, artist_input, "Spotify Streams", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'Spotify Streams', df)):,.0f}")
         st.subheader("YouTube Views:")
-        st.write(f"{(trackdetails(track_input, artist_input, "YouTube Views", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'YouTube Views', df)):,.0f}")
 
     with col5:
         st.subheader("Pandora:")
-        st.write(f"{(trackdetails(track_input, artist_input, "Pandora Streams", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'Pandora Streams', df)):,.0f}")
         st.subheader("TikTok Views:")
-        st.write(f"{(trackdetails(track_input, artist_input, "TikTok Views", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'TikTok Views', df)):,.0f}")
 
     with col6:
         st.subheader("Soundcloud:")
-        st.write(f"{(trackdetails(track_input, artist_input, "Soundcloud Streams", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'Soundcloud Streams', df)):,.0f}")
         st.subheader("All Time Rank:")
-        st.write(f"{(trackdetails(track_input, artist_input, "All Time Rank", df)):,.0f}")
+        st.write(f"{(trackdetails(track_input, artist_input, 'All Time Rank', df)):,.0f}")
 
     st.divider()
     st.header("Audio-Features")
@@ -76,18 +76,18 @@ if st.button("Track suchen"):
 
     with col7:
         st.subheader("Tempo in BPM:")
-        st.write(trackdetails(track_input, artist_input, "Tempo", df))
+        st.write(trackdetails(track_input, artist_input, 'Tempo', df))
         st.subheader("Stimmung:")
-        st.write(trackdetails(track_input, artist_input, "Stimmung", df))
+        st.write(trackdetails(track_input, artist_input, 'Stimmung', df))
 
     with col8:
         st.subheader("Tonart:")
-        st.write(trackdetails(track_input, artist_input, "Key", df), trackdetails(track_input, artist_input, "Mode", df))
+        st.write(trackdetails(track_input, artist_input, 'Key', df), trackdetails(track_input, artist_input, 'Mode', df))
         st.subheader("Energy:")
-        st.write(trackdetails(track_input, artist_input, "Energy", df))
+        st.write(trackdetails(track_input, artist_input, 'Energy', df))
 
     with col9:
         st.subheader(" ")
         st.markdown("""<span style='color:black'>leer</span>""", unsafe_allow_html=True)
         st.subheader("Danceability:")
-        st.write(trackdetails(track_input, artist_input, "Danceability", df))
+        st.write(trackdetails(track_input, artist_input, 'Danceability', df))
